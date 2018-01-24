5b623a2f4253645c18cba6ea9fe3351097fae7a4
Li Linhao <lhli@thoughtworks.com>
2018-01-24 23:05:55 +0800

    add some images

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { connect } from 'dva';
-import styles from './IndexPage.css';
+import {connect} from 'dva';
+import './IndexPage.css';
+import {Carousel} from 'antd';
+import image1 from '../assets/moe/1.jpg';
+import image2 from '../assets/moe/2.jpg';
+import image3 from '../assets/moe/3.jpg';
+import image4 from '../assets/moe/4.jpg';
 
 function IndexPage() {
+
+  const imageStyle = {width: '400px'};
+
+  const carouselContainerStyle = {width: '400px', height: '800px', textAlign: 'center', margin: '3em auto'};
+
   return (
-    <div className={styles.normal}>
-      哈哈哈哈哈
+    <div className='normal' style={carouselContainerStyle}>
+      <Carousel autoplay>
+        <div className="block1"><img alt="" src={image1} style={imageStyle} className='image1'/></div>
+        <div className="block2"><img alt="" src={image2} style={imageStyle} className='image2'/></div>
+        <div className="block3"><img alt="" src={image3} style={imageStyle} className='image3'/></div>
+        <div className="block4"><img alt="" src={image4} style={imageStyle} className='image4'/></div>
+      </Carousel>
     </div>
   );
 }
 
-IndexPage.propTypes = {
-};
+IndexPage.propTypes = {};
 
 export default connect()(IndexPage);
