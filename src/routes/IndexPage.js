86ab0edb8d960d135a98b2f02f03a9116875742b
Li Linhao <lhli@thoughtworks.com>
2018-02-28 13:25:01 +0800

    add random sagiri emoji

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -6,23 +6,58 @@ import image1 from '../assets/moe/1.jpg';
 import image2 from '../assets/moe/2.jpg';
 import image3 from '../assets/moe/3.jpg';
 import image4 from '../assets/moe/4.jpg';
+import {emojiImage} from '../utils/emojiImageMapping';
 
-function IndexPage() {
+export class IndexPage extends React.Component {
 
-  const imageStyle = {width: '400px'};
+  constructor() {
+    super();
 
-  const carouselContainerStyle = {width: '400px', textAlign: 'center', margin: '2em auto'};
+    this.state = {
+      emojiIndex: Date.now() % 30
+    };
 
-  return (
-    <div className='normal' style={carouselContainerStyle}>
-      <Carousel autoplay>
-        <div className="block1"><img alt="" src={image1} style={imageStyle} className='image1'/></div>
-        <div className="block2"><img alt="" src={image2} style={imageStyle} className='image2'/></div>
-        <div className="block3"><img alt="" src={image3} style={imageStyle} className='image3'/></div>
-        <div className="block4"><img alt="" src={image4} style={imageStyle} className='image4'/></div>
-      </Carousel>
-    </div>
-  );
+    this.emojiClickHandler = ::this.emojiClickHandler
+  }
+
+  emojiClickHandler() {
+    let newEmojiIndex = Date.now() % 30;
+    while (newEmojiIndex === this.state.emojiIndex) {
+      newEmojiIndex = Date.now() % 30
+    }
+
+    this.setState({emojiIndex: newEmojiIndex});
+  };
+
+  render() {
+
+
+    const emojiTodayStyle = {textAlign: 'center', margin: '2em auto'};
+    let emojiIndex = Date.now() % 30;
+
+    const imageStyle = {width: '400px'};
+    const carouselContainerStyle = {width: '400px', textAlign: 'center', margin: '2em auto', display: 'none'};
+
+    const getTodayEmoji = () => {
+      return emojiImage[emojiIndex]
+    };
+
+    return ([
+      <div className="emoji-today" style={emojiTodayStyle} key="emoji">
+        <a onClick={this.emojiClickHandler}>
+          <img alt="" src={getTodayEmoji()} className='emoji'/>
+        </a>
+      </div>,
+      <div className='normal' style={carouselContainerStyle} key="carousel">
+        <Carousel autoplay>
+          <div className="block1"><img alt="" src={image1} style={imageStyle} className='image1'/></div>
+          <div className="block2"><img alt="" src={image2} style={imageStyle} className='image2'/></div>
+          <div className="block3"><img alt="" src={image3} style={imageStyle} className='image3'/></div>
+          <div className="block4"><img alt="" src={image4} style={imageStyle} className='image4'/></div>
+        </Carousel>
+      </div>
+    ]);
+  }
 }
 
 IndexPage.propTypes = {};
